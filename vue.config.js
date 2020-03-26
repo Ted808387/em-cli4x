bef362d2df5f8aecbcaf8ec80db6e220338a5d0b
Ted808387 <58389259+Ted808387@users.noreply.github.com>
2020-03-26 17:11:00 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'  ? '/em-cli4x/' : '/',
-  devServer: {
-    proxy: {
-      '/API': {
-        target: 'https://vue-course-api.hexschool.io', 
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/API': '/API'
-        }
-      }
-    }
-  }
 }
 
