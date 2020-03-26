582b60699a2798dafd7819c61354758c5185690b
Ted808387 <58389259+Ted808387@users.noreply.github.com>
2020-03-26 10:54:39 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -109,8 +109,6 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
-  base: '/EM-vue-cli4',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/vue-cli4-em/'
+  publicPath: './'
 }
